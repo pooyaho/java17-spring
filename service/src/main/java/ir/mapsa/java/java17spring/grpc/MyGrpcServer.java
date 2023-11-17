ff134cb75e4b5e497a1e650a0463aee7ea7cf701
pooya.husseini@gmail.com <2fQGUsbR2zTnWhY>
2023-11-17 14:19:58 +0330

    Spring repo added to project

---
+++ service/src/main/java/ir/mapsa/java/java17spring/grpc/MyGrpcServer.java
@@ -0,0 +1,28 @@
+package ir.mapsa.java.java17spring.grpc;
+
+
+import ir.mapsa.services.MicroServiceGrpc;
+import ir.mapsa.services.RequestPr;
+import ir.mapsa.services.ResponsePr;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyGrpcServer extends  MicroServiceGrpc.MicroServiceImplBase{
+    @Override
+    public void service1(RequestPr request, io.grpc.stub.StreamObserver<ResponsePr> responseObserver) {
+        ResponsePr response = ResponsePr.newBuilder()
+                .setPayload("Hello ")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void service2(RequestPr request, io.grpc.stub.StreamObserver<ResponsePr> responseObserver) {
+    }
+
+    @Override
+    public void service3(RequestPr request, io.grpc.stub.StreamObserver<ResponsePr> responseObserver) {
+
+    }
+}
---
+++ service/src/main/java/ir/mapsa/java/java17spring/launcher/GrpcConfiguration.java
@@ -0,0 +1,51 @@
+package ir.mapsa.java.java17spring.launcher;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
+import ir.mapsa.java.java17spring.grpc.MyGrpcServer;
+import ir.mapsa.services.MicroServiceGrpc;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class GrpcConfiguration {
+    private Server server;
+    @Bean
+    public Server grpcServer(MyGrpcServer service) throws IOException {
+        server = ServerBuilder.forPort(8089)
+                .addService(service)
+                .executor(Executors.newFixedThreadPool(400))
+                .addService(ProtoReflectionService.newInstance())
+                .build()
+                .start();
+        return server;
+    }
+
+    @PreDestroy
+    public void stop() {
+        server.shutdown();
+    }
+
+    @Bean
+    public MicroServiceGrpc.MicroServiceBlockingStub clientStub() {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8089")
+                .disableRetry()
+                .executor(Executors.newFixedThreadPool(5))
+                .keepAliveTime(10, TimeUnit.SECONDS)
+                .keepAliveTimeout(5, TimeUnit.SECONDS)
+                .idleTimeout(10, TimeUnit.MINUTES)
+                .usePlaintext()
+                .build();
+
+        return MicroServiceGrpc.newBlockingStub(channel)
+                ;
+    }
+}
